239a8a8e49246b282d646fecad701588892b4613
Enrique-Nguyen <nguyenphunguyen2005@gmail.com>
2025-12-10 23:22:48 +0700

    [Refactor] Add constant app colors

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const background = Color(0xFFF5F7FA);
+  static const primaryBlue = Color(0xFF2E3A8C);
+  static const textPrimary = Color(0xFF1A1F36);
+  static const textGrey = Color(0xFF5E6C84);
+  
+  // Màu cho tính năng Funds
+  static const successGreen = Color(0xFF00C853);
+  static const errorRed = Color(0xFFFF5252);
+  static const bgRedLight = Color(0xFFFFEBEE);
+}
