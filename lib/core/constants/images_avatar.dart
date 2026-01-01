13580647b50fac1b8f3db4ef8ad68b0320b284b9
Le Duc Nguyen <leducnguyen07092005@gmail.com>
2026-01-02 03:39:41 +0700

    [Update] Can edit avatar and name in class

---
+++ lib/core/constants/images_avatar.dart
@@ -0,0 +1,12 @@
+class ImagesAvatar {
+  ImagesAvatar._();
+
+  static const List<String> defaultAvatars = [
+    'https://api.dicebear.com/7.x/avataaars/png?seed=Felix',
+    'https://api.dicebear.com/7.x/avataaars/png?seed=Aneka',
+    'https://api.dicebear.com/7.x/bottts/png?seed=Ginger',
+    'https://api.dicebear.com/7.x/adventurer/png?seed=Midnight',
+    'https://api.dicebear.com/7.x/micah/png?seed=Leo',
+    'https://api.dicebear.com/7.x/notionists/png?seed=Bella',
+  ];
+}
